4faae7a7fd1a1c78a6faebc85b9b429d3d5e6b6d
Elliott Williams <e@elliottwillia.ms>
2014-04-16 23:08:32 -0400

    Fixed thanks url

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('rsvp.views',
-    url(r'(?P<slug>[A-Za-z0-9_-]+)/$', 'event_view', name='rsvp_event_view'),
-    url(r'(?P<slug>[A-Za-z0-9_-]+)/thanks/(?P<guest_id>\d+)/$', 'event_thanks', name='rsvp_event_thanks'),
+    url(r'^(?P<slug>[A-Za-z0-9_-]+)/$', 'event_view', name='rsvp_event_view'),
+    url(r'^(?P<slug>[A-Za-z0-9_-]+)/thanks/(?P<guest_id>\d+)/$', 'event_thanks', name='rsvp_event_thanks'),
 )
